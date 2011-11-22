813b98cbdaccf3932df8537ce6e327266a709e66
Sterling Cobb <sterlingcobb@gmail.com>
2011-11-21 23:04:13 -0700

    added blank rails project

---
+++ learning_sencha/sencha_forms/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SenchaForms::Application.config.secret_token = 'dcf9c6a5df26c51693e413bb1a543a1850f0765ff6377e518e6b2b926f0c50931a2924b02ea6124cd6b865b53fbcc27cef6d68d9af6709963d235a716cc8bec8'
